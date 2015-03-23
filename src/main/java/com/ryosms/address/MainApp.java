ad5b3daa729fec6b073e35cd5ae711cede1d9cbe
ryosms <ryo.sms@gmail.com>
2015-03-23 16:10:59 +0900

    Add list's data to MainApp and add dummy data

--- src/main/java/com/ryosms/address/MainApp.java
+++ src/main/java/com/ryosms/address/MainApp.java
@@ -1,6 +1,9 @@
 package com.ryosms.address;
 
+import com.ryosms.address.model.Person;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -17,7 +20,28 @@ public class MainApp extends Application {
     private Stage primaryStage;
     private BorderPane rootLayout;
 
-    @Override
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    /**
+     * ダミーデータを追加する
+     */
+    public MainApp() {
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+
     public void start(Stage stage) throws Exception {
         this.primaryStage = stage;
         this.primaryStage.setTitle("AddressApp");
